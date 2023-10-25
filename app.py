342b06540fcdab922ca59cec5bb744926e2676e7
Zin Yan <zwy@mit.com.mm>
2023-10-25 16:43:09 +0630

    fast api

--- app.py
+++ app.py
@@ -1,49 +1,15 @@
 from pandasai import SmartDataframe
 from langchain.llms import OpenAI
 import pandas as pd
-# import json
-# import cachetools
-from flask import Flask, jsonify, request
-from cachetools import LRUCache  # Import cachetools for caching
+from pandasai.llm import OpenAI
 
-# Load data and initialize SmartDataframe
-df = pd.read_excel('A.L.I2.xlsx')
 
-# Create a cache using cachetools
-cache = LRUCache(maxsize=1000)  # You can adjust the cache size as needed
 
-# Create a dictionary containing the API token
-model_kwargs = {
-    "api_token": "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"
-}
+df =  pd.read_excel('A.L.I2.xlsx')
 
-# Initialize the OpenAI object with model_kwargs
-llm = OpenAI(model_kwargs=model_kwargs)
+llm = OpenAI(api_token="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")
+sdf = SmartDataframe(df, config={"llm": llm})
+
 langchain_sdf = SmartDataframe(df, config={"llm": llm})
-
-
-app = Flask(__name__)
-
-@app.route('/chatbot', methods=['POST', 'GET'])
-def main():
-    data = request.json
-    query = data.get('query')
-    
-    try:
-       
-
-        # langchain_sdf = SmartDataframe(df, config={"llm": llm})
-        query = "show me the apt that has both swimming pool and gym"
-        output = ((langchain_sdf.chat(f" pls output me with pandas dataframe, 0 means theere is no [column name]  1 means that Apartments has [columns name].... {query} ")))
-        
-        # Convert the output to JSON format
-        # output_json = output.to_json(orient='records')  # Convert to JSON array
-        
-        # Return the JSON response
-        return jsonify({"output": output})
-        
-    except Exception as e:
-        return jsonify({"error": str(e)})
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=9494, debug=True)
+query = input(str())
+print((langchain_sdf.chat(f" pls output me with pandas dataframe, 0 means theere is no [column name]  1 means that Apartments has [columns name].... {query} ")))
---
+++ test.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel
+from pandasai.smart_dataframe import SmartDataframe
+from langchain.llms import OpenAI
+
+app = FastAPI()
+
+# Load data and initialize SmartDataframe
+df = pd.read_excel('A.L.I2.xlsx')
+
+# Create a dictionary containing the API token
+model_kwargs = {
+    "api_key": "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"
+}
+llm = OpenAI(api_key="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")
+
+langchain_sdf = SmartDataframe(df, config={"llm": llm})
+
+
+class Query(BaseModel):
+    query: str
+    api_key: str
+
+
+@app.post('/chatbot')
+async def main(item: Query):
+    if item.api_key != model_kwargs['api_key']:
+        return {"error": "Invalid API key"}
+
+    try:
+        query = item.query
+        langchain_sdf.chat(f" pls output me with pandas dataframe, 0 means there is no [column name], 1 means that Apartments have [column name].... {query}")
+
+        # Convert SmartDataframe to a regular pandas DataFrame
+        converted_df = langchain_sdf.to_pandas_dataframe()
+
+        # Convert the DataFrame to JSON
+        converted_json = converted_df.to_json(orient='records')
+
+        return {"output": converted_json}
+    except Exception as e:
+        return {"error": str(e)}
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=9494)  # Remove the 'debug' argument
